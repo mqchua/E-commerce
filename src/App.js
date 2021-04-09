30c4d34039a673b83820cc095ac3f0490694b869
Ming <chua_ming_quan@live.com>
2021-04-09 18:18:04 +0800

    added 8 products

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from 'react'
-import Products from './components/Products/Products';
+import React, {useState, useEffect} from 'react'
+import { commerce } from './lib/commerce';
+import { Products, Navbar } from './components';
 
 export const App = () => {
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+        
+        setProducts(data);
+    }
+
+    useEffect(() => { // mount component on first loading 
+        fetchProducts();
+    }, []);
+
+    console.log(products);
+
     return (
         <div>
-            <Products />
+            <Navbar />
+            <Products products={products}/>
         </div>
     )
 }
